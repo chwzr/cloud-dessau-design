1295fabbd3d11fc5475eaf0d9a11d9170a13acfa
chwzr <github@koppe.design>
2019-10-06 21:58:08 +0200

    enable push

--- components/messaging.js
+++ components/messaging.js
@@ -10,6 +10,28 @@ const Push = () => {
         console.log('Notification permission granted.');
         // TODO(developer): Retrieve an Instance ID token for use with FCM.
         // ...
+        if(process.browser){
+          var messaging = firebase.messaging();
+          messaging.usePublicVapidKey("BHfApPgxFt25VeufNN7mK9jMCVGteCZb8P5ryH4CE5GOAgprtSTE74U8HVlc2pdo-ZGbw05cjkHgzHuzihouJ74");
+          messaging.getToken().then((currentToken) => {
+            if (currentToken) {
+              console.log("token: ", currentToken)
+              window.pushtoken = currentToken;
+              // sendTokenToServer(currentToken);
+              // updateUIForPushEnabled(currentToken);
+            } else {
+              // Show permission request.
+              console.log('No Instance ID token available. Request permission to generate one.');
+              // Show permission UI.
+              // updateUIForPushPermissionRequired();
+              // setTokenSentToServer(false);
+            }
+          }).catch((err) => {
+            console.log('An error occurred while retrieving token. ', err);
+            // showToken('Error retrieving Instance ID token. ', err);
+            // setTokenSentToServer(false);
+          });
+        }
       } else {
         console.log('Unable to get permission to notify.');
       }
